c33db3741a71a74111e69ee5c98b192c80b59546
Samuel-Ampuero <sampuero@byu.edu>
2025-02-07 14:31:19 -0700

    fixed file paths in certain components. Got rid of TS types in FindRelative

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,9 +5,9 @@ export default function Home(){
     return(
     <main className="container my-4 flex-grow-1 d-flex flex-column">
       <h1 className="d-flex justify-content-center">Welcome to Funeral Memories</h1>
-      <button className="btn btn-primary mt"> <a href="instructions.html" className="text-white text-decoration-none">Click here if you're new! (instructions)</a></button>
+      <button className="btn btn-primary mt"> <a href="/instructions" className="text-white text-decoration-none">Click here if you're new! (instructions)</a></button>
       <br/><br/>
-      <form method="get" action="./wall.html">
+      <form method="get" action="/wall">
         <div className="mb-3">
           <label for="name" className="form-label">Name:</label>
           <input type="text" id="name" className="form-control" placeholder="Name" required/>
@@ -17,7 +17,7 @@ export default function Home(){
           <button type="submit" className="btn btn-primary mt-2">Join</button>
         </div>
       </form>
-      <button type="submit" className="btn btn-secondary"><a href="./instructions.html" className="text-white text-decoration-none">Host</a></button>
+      <button type="submit" className="btn btn-secondary"><a href="/find-relative" className="text-white text-decoration-none">Host</a></button>
     </main>
     );
 }
