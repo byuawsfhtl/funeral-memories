ef25c549afd35f33a097d1d5653dc6643abb637c
Samuel-Ampuero <sampuero@byu.edu>
2025-04-03 11:41:25 -0600

    Updates for RootsTech

--- service/database.js
+++ service/database.js
@@ -1,10 +1,10 @@
-const { MongoClient } = require('mongodb');
-const config = require('./dbConfig.json');
+const { MongoClient } = require("mongodb");
+const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('FuneralMemories');
-const memoryCollection = db.collection('memories');
+const db = client.db("FuneralMemories");
+const memoryCollection = db.collection("memories");
 
 // async function main() {
 //   (async function testConnection() {
@@ -25,33 +25,48 @@ const memoryCollection = db.collection('memories');
 // main().catch(console.error);
 
 (async function testConnection() {
-    await client.connect();
-    await db.command({ ping: 1 });
-  })().catch((ex) => {
-    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-    process.exit(1);
+	await client.connect();
+	await db.command({ ping: 1 });
+})().catch((ex) => {
+	console.log(
+		`Unable to connect to database with ${url} because ${ex.message}`
+	);
+	process.exit(1);
 });
 
-async function addMemory(memory){
-  try {
-    const result = await memoryCollection.insertOne(memory);
-    const inserted = await memoryCollection.findOne({ _id: result.insertedId });
-    return inserted;
-  } catch (err) {
-    console.error('Error adding memory to database: ', err.message);
-    throw new Error('Unable to add memory');
-  }
+async function addMemory(memory) {
+	try {
+		const result = await memoryCollection.insertOne(memory);
+		const inserted = await memoryCollection.findOne({ _id: result.insertedId });
+		return inserted;
+	} catch (err) {
+		console.error("Error adding memory to database: ", err.message);
+		throw new Error("Unable to add memory");
+	}
 }
 
-async function getMemories(){
-  try {
-      const memories = await memoryCollection.find({});
-      const array = await memories.toArray();
-      return array;
-  } catch (err) {
-      console.error("Error getting memories from the database: ", err.message);
-      throw new Error('Unable to get memories');
-  }
+// TODO:: Need to make sure this is the correct way to implement it
+
+// async function deleteMemory(memory) {
+// 	try {
+// 		const result = await memoryCollection.deleteOne(memory);
+// 		return true;
+// 	} catch (err) {
+// 		console.error("Error deleting memory in database: ", err.message);
+// 		throw new Error("Unable to delete memory");
+// 	}
+// }
+
+async function getMemories(groupId) {
+	try {
+		const memories = await memoryCollection
+			.find({ groupId: groupId })
+			.toArray();
+		return memories;
+	} catch (err) {
+		console.error("Error getting memories from the database: ", err.message);
+		throw new Error("Unable to get memories");
+	}
 }
 
 module.exports = { addMemory, getMemories };
