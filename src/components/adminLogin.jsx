7d3ff445442d57b27b7fac99ae1d1020b34c0d54
jocelynperucca <jocelynperucca@gmail.com>
2025-05-27 13:51:53 -0600

    admin login

---
+++ src/components/adminLogin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminLogin() {
+  return (
+    <main className="container my-5 text-center">
+      <h2>Admin Login</h2>
+      <p>TODO: Add Group ID and password verification here.</p>
+    </main>
+  );
+}
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,24 +1,26 @@
-import { StrictMode } from 'react'
-import React from 'react';
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
 // import './index.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from './App.jsx'
-import Header from './Header.jsx'
-import Home from './Home.jsx';
-import Login from './Login.jsx';
-import Instructions from './Instructions.jsx';
-import Confirmation from './Confirmation.jsx';
-import Wall from './Wall.jsx';
-import AddMem from './AddMem.jsx';
-import Footer from './Footer.jsx';
-import Quote from './Quote.jsx';
-import Join from './Join.jsx';
-import Host from './Host.jsx';
-import FindRelative from './find_relative/FindRelative.jsx';
-import { WebSocketChat } from './Chat.jsx';
+import App from "./App.jsx";
+import Header from "./Header.jsx";
+import Home from "./Home.jsx";
+import Login from "./Login.jsx";
+import Instructions from "./Instructions.jsx";
+import Confirmation from "./Confirmation.jsx";
+import Wall from "./Wall.jsx";
+import AddMem from "./AddMem.jsx";
+import Footer from "./Footer.jsx";
+import Quote from "./Quote.jsx";
+import Join from "./Join.jsx";
+import Host from "./Host.jsx";
+import RoleSelect from "./RoleSelect.jsx";
+import AdminLogin from "./adminLogin.jsx";
+import FindRelative from "./find_relative/FindRelative.jsx";
+import { WebSocketChat } from "./Chat.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode className="d-flex flex-column min-vh-100">
     <Router>
       <Header />
@@ -30,13 +32,15 @@ createRoot(document.getElementById('root')).render(
         <Route path="/wall" Component={Wall} />
         <Route path="/addmem" Component={AddMem} />
         <Route path="/quote" Component={Quote} />
-/*      <Route path="/chat" Component={WebSocketChat} /> */
+        /* <Route path="/chat" Component={WebSocketChat} /> */
         <Route path="/find-relative" Component={FindRelative} />
-        <Route path="/join" element={<Join />} />    
+        <Route path="/join" element={<Join />} />
         <Route path="/host" element={<Host />} />
+        <Route path="/roleSelect" element={RoleSelect}></Route>
+        <Route path="/admin" element={Admin}></Route>
         <Route path="*" Component={Home} />
       </Routes>
       <Footer />
     </Router>
   </React.StrictMode>
-)
+);
