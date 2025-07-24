ea9f36508fde60b5081fd8a3eb3d8c396a331797
jocelynperucca <jocelynperucca@gmail.com>
2025-07-24 13:45:45 -0600

    cleanup

--- src/components/RoleSelect.tsx
+++ src/components/RoleSelect.tsx
@@ -1,35 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function RoleSelect() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	return (
-		<main
-			className="d-flex justify-content-center align-items-center flex-column container my-5"
-			style={{ minHeight: "80vh" }}
-		>
-			<div className="w-100" style={{ maxWidth: "400px" }}>
-				<h1
-					className="text-center mb-4"
-					style={{ fontFamily: "Merriweather, serif", fontWeight: 500 }}
-				>
-					Choose Your Role
-				</h1>
+  return (
+    <main
+      className="d-flex justify-content-center align-items-center flex-column container my-5"
+      style={{ minHeight: "80vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <h1
+          className="text-center mb-4"
+          style={{ fontFamily: "Merriweather, serif", fontWeight: 500 }}
+        >
+          Choose Your Role
+        </h1>
 
-				<div className="d-grid gap-3 mt-4">
-					<button className="btn btn-primary" onClick={() => navigate("/join")}>
-						Join as Guest
-					</button>
-					<button
-						className="btn btn-outline-primary"
-						onClick={() => navigate("/admin")}
-					>
-						Join as Admin
-					</button>
-				</div>
-			</div>
-		</main>
-	);
+        <div className="d-grid gap-3 mt-4">
+          <button className="btn btn-primary" onClick={() => navigate("/join")}>
+            Join as Guest
+          </button>
+          <button
+            className="btn btn-outline-primary"
+            onClick={() => navigate("/admin")}
+          >
+            Join as Admin
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
