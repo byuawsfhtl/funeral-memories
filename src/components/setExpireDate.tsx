91dc77c98146e69c817eee8a1a063308e618063c
jocelynperucca <jocelynperucca@gmail.com>
2025-08-11 14:42:32 -0600

    try again

--- src/components/setExpireDate.tsx
+++ src/components/setExpireDate.tsx
@@ -68,6 +68,7 @@ export default function SetExpireDate() {
         display: "flex",
         justifyContent: "center", // center vertically
         alignItems: "center", // center horizontally
+        transform: "translateY(-10%)",
       }}
     >
       <div style={{ width: "100%", maxWidth: 400 }}>
