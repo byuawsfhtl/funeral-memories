e5642adfa2352c733baacbcffbece2f30aac9f0c
Samuel-Ampuero <sampuero@byu.edu>
2025-10-15 14:47:20 -0600

    more todo comments

--- api/auth.ts
+++ api/auth.ts
@@ -2,14 +2,15 @@
 import axios from "axios";
 
 export async function fetchAndStoreToken() {
-  const authTokenUrl = "https://auth.fhtl.org/get_token";
-  try {
-    const response = await axios.post(authTokenUrl);
-    const token = response.data.access_token;
-    sessionStorage.setItem("yourKey", token);
-    return token;
-  } catch (error) {
-    console.error("Error fetching token:", error);
-    throw error;
-  }
+	//TODO:: Change api to be a functoin in service file
+	const authTokenUrl = "https://auth.fhtl.org/get_token";
+	try {
+		const response = await axios.post(authTokenUrl);
+		const token = response.data.access_token;
+		sessionStorage.setItem("yourKey", token);
+		return token;
+	} catch (error) {
+		console.error("Error fetching token:", error);
+		throw error;
+	}
 }
