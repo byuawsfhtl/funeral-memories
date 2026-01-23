3a92a4a212972ade4d6482a49442ff5c44c86934
jocelynperucca <jocelynperucca@gmail.com>
2026-01-23 14:33:12 -0700

    android wrapper for .aab for app

---
+++ android/app/src/main/java/com/funeralmemories/fhtl/MainActivity.java
@@ -0,0 +1,5 @@
+package com.funeralmemories.fhtl;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.funeralmemories.fhtl",
+  appName: "Funeral Memories",
+  webDir: "dist",
+};
+
+export default config;
