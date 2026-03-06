2a508d9ffb92b32bd9166da41257d003e06b9e3d
Samuel-Ampuero <sampuero@byu.edu>
2026-03-06 09:59:43 -0700

    Changing header icon path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 import "../styles/header.css";
 
 export default function Header() {
-  return (
-    <header>
-      <nav>
-        <img src="src\assets\navylogo.png" alt="Funeral Memories Logo" />
-        <Link to="/"><h1>Funeral Memories</h1></Link>
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/instructions">Instructions</Link>
-        </div>
-      </nav>
-    </header>
-  );
+	return (
+		<header>
+			<nav>
+				<img src={logo} alt="Funeral Memories Logo" />
+				<Link to="/">
+					<h1>Funeral Memories</h1>
+				</Link>
+				<div className="nav-links">
+					<Link to="/">Home</Link>
+					<Link to="/instructions">Instructions</Link>
+				</div>
+			</nav>
+		</header>
+	);
 }
