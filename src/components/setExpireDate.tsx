22528f732df6afd13d8fbd7eb24874d17725029d
jocelynperucca <jocelynperucca@gmail.com>
2026-03-27 15:15:24 -0600

    fixed spacing and colors

--- src/components/setExpireDate.tsx
+++ src/components/setExpireDate.tsx
@@ -63,69 +63,68 @@ export default function SetExpireDate() {
   const maxDateStr = toDatetimeLocalString(oneMonthLater);
 
   return (
-    <main
-      className="form-main"
-      >
+    <main className="form-main">
       <div>
-
         <div className="login-box">
-                  <h2 className="form-title">
-          Set Group Expiration Date
-        </h2>
+          <h2 className="form-title">Set Group Expiration Date</h2>
 
-        <p className="text-muted text-center mb-2">
-          Please enter a date and time for when you want this group to expire.
-        </p>
-        <p className="text-danger text-center mb-2">
-          <strong>
-            Note: The expiration date must be within one month from today and a
-            minimum of 2 hours from the current time.
-          </strong>
-        </p>
-        <br />
-        {error && (
-          <div className="alert alert-danger text-center py-2">{error}</div>
-        )}
-
-        <form onSubmit={handleSetExpiration}>
-          <div >
-            <label className="form-label">Group Expiration Date & Time</label>
-            <br />
-            <input
-              type="datetime-local"
-              className="form-control"
-              value={expirationDateTime}
-              onChange={(e) => setExpirationDateTime(e.target.value)}
-              required
-              min={minDateStr}
-              max={maxDateStr}
-              style={{width:"50%"}}
-            />
-          </div>
+          <p className=" text-center mb-2">
+            Please enter a date and time for when you want this group to expire.
+          </p>
+          <p className="text-danger text-center mb-2">
+            <strong>
+              Note: The expiration date must be within one month from today and
+              a minimum of 2 hours from the current time.
+            </strong>
+          </p>
           <br />
-          <div className="d-grid">
-            <button
-              type="submit"
-              className="btn btn-secondary btn-lg"
-              style={{
-							  backgroundColor: hostHover
-								  ? "#153443"
-								  : "#1C495E",
-							  color: "#fff",
-							  transition: "background 0.2s, border-color 0.2s",
-                fontFamily: "DMSans"
-						  }}
-              onMouseEnter={() => setHostHover(true)}
-							onMouseLeave={() => setHostHover(false)}
-            >
-              Set Expiration & Continue
-            </button>
-          </div>
-        </form>
+          {error && (
+            <div className="alert alert-danger text-center py-2">{error}</div>
+          )}
+
+          <form onSubmit={handleSetExpiration}>
+            <div>
+              <label className="form-label">Group Expiration Date & Time</label>
+              <br />
+              <input
+                type="datetime-local"
+                className="form-control"
+                value={expirationDateTime}
+                onChange={(e) => setExpirationDateTime(e.target.value)}
+                required
+                min={minDateStr}
+                max={maxDateStr}
+                style={{ width: "50%" }}
+              />
+            </div>
+            <br />
+            <div className="d-grid">
+              <button
+                type="submit"
+                className="btn btn-secondary btn-lg"
+                style={{
+                  backgroundColor: hostHover ? "#153443" : "#1C495E",
+                  color: "#fff",
+                  transition: "background 0.2s, border-color 0.2s",
+                  fontFamily: "DMSans",
+                }}
+                onMouseEnter={() => setHostHover(true)}
+                onMouseLeave={() => setHostHover(false)}
+              >
+                Set Expiration & Continue
+              </button>
+            </div>
+          </form>
         </div>
 
-        <br /><br /><br /><br /><br /><br /><br /><br />
-
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
     </main>
   );
